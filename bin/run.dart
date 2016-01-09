c7b12f99eab859eeaa11f716c99c3d16eef93255
Kenneth Endfinger <kaendfinger@gmail.com>
2016-01-09 14:53:35 -0500

    Geoquery - Near: Use stream.

--- bin/run.dart
+++ bin/run.dart
@@ -279,7 +279,7 @@ class MongoDatabaseHistorianAdapter extends HistorianDatabaseAdapter {
           "type": "dynamic"
         }
       ],
-      r"$result": "table",
+      r"$result": "stream",
       r"$invokable": "read"
     });
 
